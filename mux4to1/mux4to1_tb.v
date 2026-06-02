93f9af6caca146580cdde281763e4c65ed6713d5
Tyler McClure <tylermcclure11@gmail.com>
2026-06-02 13:40:39 -0400

    testbench improvements, full coverage coming next

--- mux4to1/mux4to1_tb.v
+++ mux4to1/mux4to1_tb.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps
+
 module mux4to1_tb;
     reg s0, s1;
     reg d0, d1, d2, d3;
@@ -18,13 +20,13 @@ module mux4to1_tb;
         d2 = 0;
         d3 = 1;
 
-        s1 = 0; s0 = 0; #10
+        s1 = 0; s0 = 0; #10;
         if (out != d0) $display("ERROR: 00 should output d0");
-        s1 = 0; s0 = 1; #10
+        s1 = 0; s0 = 1; #10;
         if (out != d1) $display("ERROR: 01 should output d1");
-        s1 = 1; s0 = 0; #10
+        s1 = 1; s0 = 0; #10;
         if (out != d2) $display("ERROR: 10 should output d2");
-        s1 = 1; s0 = 1; #10
+        s1 = 1; s0 = 1; #10;
         if (out != d3) $display("ERROR: 11 should output d3");
 
         $display("Test completed.");
