58f685ed8124bc0b28a700dab576885ef7750c49
Tyler McClure <tylermcclure11@gmail.com>
2026-06-02 12:59:04 -0400

    simplified 4:1 mux

---
+++ mux4to1/mux4to1.v
@@ -0,0 +1,16 @@
+module mux4to1 (
+    input s0,
+    input s1,
+    input d0,
+    input d1,
+    input d2,
+    input d3,
+    output out
+);
+
+    assign out = (~s1 & ~s0 & d0)
+               | (~s1 & s0 & d1)
+               | (s1 & ~s0 & d2)
+               | (s1 & s0 & d3);
+
+endmodule
